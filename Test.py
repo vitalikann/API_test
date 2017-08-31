156cfe92e3ac35c5ba9b30b4ccac5425174460ae
vitalikann <31486323+vitalikann@users.noreply.github.com>
2017-08-31 16:24:56 +0300

    Add files via upload

--- Test.py
+++ Test.py
@@ -1,33 +1,71 @@
 import requests
 import json
+
 from jsonschema import validate
 
+def validate_json_chema():
+    return
+
+def test_login():
 # data for request
-url = "http://qapediaq.webprv.com/api/auth/login"
+    url = "http://qapediaq.webprv.com/api/auth/login"
 
-header = {
-        "Version": "28"
-        }
+    header = {
+            "Version": "28"
+            }
 
-data = {
-        "email": "2kec@gmail.com",
-        "password": "12345678"
-        }
+    data = {
+            "email": "2kec@gmail.com",
+            "password": "12345678"
+            }
+
 # getting response
-response = requests.post(url, data=data, headers=header)
+    response = requests.post(url, data=data, headers=header)
 
 # extract data from response and saving token for the next test
-token = response.json()["token"]
+    global token
+    token = response.json()["token"]
 
 # open and load json scheme file
-schema_file = open("login.json", "r")
-schema_json = json.load(schema_file)
+    schema_file = open("login.json", "r")
+    schema_json = json.load(schema_file)
 
 # validation
-validate(response.json(), schema_json)
+    validate(response.json(), schema_json)
 
 # close json scheme file
-schema_file.close()
+    schema_file.close()
 
+    return token
 
+def test_addchild():
+# data for request
+    url = "http://qapediaq.webprv.com/api/parent/addchild"
 
+    header = {
+        "Version": "28"
+    }
+
+    data = {
+        "token": token,
+        "birthday": "2010-10-16",
+        "first_name": "test first name",
+        "gender": "female",
+        "last_name": "test last name"
+    }
+
+# getting response
+    response = requests.post(url, data=data, headers=header)
+
+# open and load json scheme file
+    schema_file = open("addchild.json", "r")
+    schema_json = json.load(schema_file)
+
+# validation
+    validate(response.json(), schema_json)
+
+# close json scheme file
+    schema_file.close()
+
+test_login()
+test_addchild()
